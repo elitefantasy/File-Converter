5d43cd0cdda131f8cf3f1e3e987820b40f486db6
elitefantasy <81807725+elitefantasy@users.noreply.github.com>
2022-07-11 20:53:31 +0530

    compiler update

---
+++ compileProgram.bat
@@ -0,0 +1,3 @@
+@REM powershell -noexit "& ""E:\3 Programing\Github\File-Converter\compileProgram.ps1"""
+powershell -ExecutionPolicy ByPass -File "e:\3 Programing\Github\File-Converter\compileProgram.ps1"
+pause
