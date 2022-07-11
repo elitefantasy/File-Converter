9b491ab86458ea17ff3cdbbb3b04758af7ea2cb9
elitefantasy <81807725+elitefantasy@users.noreply.github.com>
2022-07-12 00:50:11 +0530

    Update compileProgram.ps1

--- compileProgram.ps1
+++ compileProgram.ps1
@@ -1,13 +1,14 @@
 # Make sure to run in powershell
 
-cd "E:\3 Programing\Github\File-Converter"
+Set-Location "E:\3 Programing\Github\File-Converter"
 
 .\venv\Scripts\Activate.ps1
 
-Remove-Item 'build' -Recurse;Remove-Item 'dist' -Recurse
+Remove-Item 'dist' -Recurse
 
 pyinstaller --noconfirm --onedir --windowed --icon "E:/3 Programing/Github/File-Converter/icons/convert.ico" --add-data "E:/3 Programing/Github/File-Converter/icons;icons/" --add-data "E:/3 Programing/Github/File-Converter/poppler-22.04.0;poppler-22.04.0/" --add-data "E:/3 Programing/Github/File-Converter/Version.txt;." --add-data "E:/3 Programing/Github/File-Converter/FileConverter_UI.ui;."  "E:/3 Programing/Github/File-Converter/File_converter.py"
 
 Remove-Item 'build' -Recurse
-del File_converter.spec
+Remove-Item File_converter.spec
+& "E:\3 Programing\Github\File-Converter\setup\File_Converter.iss"
 # pause
