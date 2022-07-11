1e1e065f6cf6e00b5510d074bc0e765e3e0edf02
elitefantasy <81807725+elitefantasy@users.noreply.github.com>
2022-07-11 20:05:27 +0530

    test

--- test.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-import os
-# path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
-# print(path_to_download_folder)
-
-print(str(os.path.join(Path.home())))
